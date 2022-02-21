945035e1834448387da4e8b752e0bc97269ca126
Jan Henning <jth-github@posteo.de>
2022-02-21 19:48:01 +0100

    Add/Enable elfeed

---
+++ personal/elfeed.el
@@ -0,0 +1,13 @@
+(require 'prelude-packages)
+
+;;; elfeed
+(prelude-require-packages '(elfeed elfeed-org))
+(setq rmh-elfeed-org-files (list "~/.emacs.d/personal/elfeed.org"))
+
+(elfeed-org)
+(define-key elfeed-search-mode-map (kbd "R") 'elfeed-mark-all-as-read)
+
+(defun elfeed-mark-all-as-read ()
+  (interactive)
+  (mark-whole-buffer)
+  (elfeed-search-untag-all-unread))
