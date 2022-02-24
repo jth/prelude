4790e680ce029b192e23d639f93a32e6f958ef10
Jan Henning <jth-github@posteo.de>
2022-02-24 22:53:49 +0100

    Use UTF-8 by default, change backup files dir, show lines >120 chars

--- personal/general.el
+++ personal/general.el
@@ -1,2 +1,14 @@
+;; Prefer UTF-8 encoding
 (prefer-coding-system 'utf-8)
 (set-language-environment 'utf-8)
+
+;; Save backup files to tmp dir. Doesn't clutter file tree
+;; and avoids iCloud warning popups on Windows
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+;; Highlight overly long lines
+(require 'whitespace)
+(setq whitespace-line-column 120)
