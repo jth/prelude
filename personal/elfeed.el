5293f4c9fad08e9e4295dad6ce2f23d3ad301993
Jan Henning <jth-github@posteo.de>
2022-02-27 14:52:22 +0100

    Move resource files to resource/ folder within personal/

--- personal/elfeed.el
+++ personal/elfeed.el
@@ -2,7 +2,7 @@
 
 ;;; elfeed
 (prelude-require-packages '(elfeed elfeed-org))
-(setq rmh-elfeed-org-files (list "~/.emacs.d/personal/elfeed.org"))
+(setq rmh-elfeed-org-files (list "~/.emacs.d/personal/resource/elfeed.org"))
 
 (elfeed-org)
 (define-key elfeed-search-mode-map (kbd "R") 'elfeed-mark-all-as-read)
