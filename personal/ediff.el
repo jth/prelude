d09ea01a4d0c97a185ec7642cfd899853562e902
Jan Henning <jth-github@posteo.de>
2022-03-16 21:38:57 +0100

    Minor magit / ediff changes

---
+++ personal/ediff.el
@@ -0,0 +1,4 @@
+(require 'ediff)
+
+(setq ediff-split-window-function #'split-window-horizontally)
+(setq ediff-window-setup-function #'ediff-setup-windows-plain)
--- personal/magit.el
+++ personal/magit.el
@@ -1,3 +1,6 @@
+(require 'magit)
+(require 'magit-ediff)
+
 (defun magit-yank-branch-name ()
   "Show the current branch in the echo-area and adds it to the `kill-ring'."
   (interactive)
@@ -6,3 +9,8 @@
         (progn (kill-new branch)
                (message "%s" branch))
       (user-error "No current branch"))))
+
+(setq magit-diff-refine-hunk (quote all))
+(setq magit-ediff-dwim-show-on-hunks t)
+;; automatically refresh magit buffers
+(add-hook 'after-save-hook 'magit-after-save-refresh-status t)
