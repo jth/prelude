a73f4e408b0a137dd105ed17f04af4b7d111a60c
Jan Henning <jth-github@posteo.de>
2022-03-27 23:02:19 +0200

    Enable code folding for rust

--- personal/rust.el
+++ personal/rust.el
@@ -1,8 +1,15 @@
 (require 'prelude-packages)
 
 ;;; rustic
-(prelude-require-packages '(rustic))
+(prelude-require-packages '(rustic lsp-origami))
 (setq rustic-format-on-save t)
+(setq lsp-ui-imenu-auto-refresh 't)
+(setq lsp-ui-imenu-auto-refresh-delay 0.8)
+
+;; code-folding using origami
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-after-open-hook #'lsp-origami-try-enable)
+  (define-key lsp-mode-map (kbd "<backtab>") #'origami-recursively-toggle-node))
 
 ;; custom bindings
 (global-set-key [f8] 'treemacs)
