50919804adf91a709282c9539a61a3e6b77b73ff
Jan Thilo Henning <jan.thilo.henning@sap.com>
2022-03-29 09:49:02 +0200

    Enable single-char boldness for org mode

--- personal/org.el
+++ personal/org.el
@@ -12,3 +12,9 @@
           (lambda ()
             (when org-inline-image-overlays
               (org-redisplay-inline-images))))
+
+;;; Allow highlighting single char
+;;; https://stackoverflow.com/questions/1218238/how-to-make-part-of-a-word-bold-in-org-mode
+(setcar org-emphasis-regexp-components " \t('\"{[:alpha:]")
+(setcar (nthcdr 1 org-emphasis-regexp-components) "[:alpha:]- \t.,:!?;'\")}\\")
+(org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
